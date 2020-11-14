317a00c1d016073dcf34e3325a500b0ce9919aa6
Chris Desir <cd2902@nyu.edu>
2020-11-14 10:05:16 -0800

    Remove Test Test Files
    Signed-off-by: Chris Desir <cd2902@nyu.edu>

--- GiftcardSite/tests/test_SQLUnion.py
+++ GiftcardSite/tests/test_SQLUnion.py
@@ -1,6 +1,34 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from LegacySite.models import *
+from LegacySite.views import *
+from django.http import HttpRequest
+from bs4 import BeautifulSoup
 
-response = self.app.get('/login', base_url='https://localhost')
-assert(response.status_code == 200)
 
 
-self.Card1 = Card.objects.create( data = "test", prod = 1, fp = "test", amount = 1000, user="test")
+import json
+
+class TestResponse(TestCase):
+     def setUp(self):
+          self.client = Client()
+
+     def test_correct_autoescaping(self):
+          Card1 = Card.objects.create( data = """{"merchant_id": "NYU Apparel Card", "customer_id": "chrisd", "total_value": "1000", "records": [{"record_type": "amount_change", "amount_added": 2000, "signature": "[ insert crypto signature here ]"}]}""", prod = 1, fp = "test", amount = 1000, user="chrisd")
+          response = self.client.post(reverse('Use a card'))
+          print(response)
+          soup = BeautifulSoup(response.content)
+          print(soup)
+          testsoupfind = soup.find('input', {'admin'})
+          print(response.content)
+          print(testsoupfind)
+
+
+
+
+
+
+
+
+
+
